c47864f9ef9c15d83472974cee3ec66a27acfbbc
michaelanova <mmiissaann@seznam.cz>
2017-11-07 17:38:43 +0100

    login and signup forms, modals

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   uid: DS.attr('string'),
   username: DS.attr('string'),
-  avatar: DS.attr('string'),
-  posts: DS.hasMany('post'),
-  comments: DS.hasMany('comment')
+  //avatar: DS.attr('string'),
+  posts: DS.hasMany('article'),
+  //comments: DS.hasMany('comment')
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,24 +4,46 @@ import Ember from 'ember';
 export default Route.extend({
   firebaseApp: Ember.inject.service(),
   session: Ember.inject.service(),
+  signInvisible: true,
+  signUpvisible: false,
   beforeModel: function() {
     return this.get('session').fetch().catch(function() {});
   },
   actions: {
+    signInOpen() {
+      var controller = this.get('controller');
+      controller.set('signInvisible', true);
+      this.set('signInvisible', true);
+    },
+    signUpOpen() {
+      this.set('signUpvisible', true);
+      var controller = this.get('controller');
+      controller.set('signUpvisible', true);
+    },
     signIn: function(provider) {
+      var controller = this.get('controller');
+         var email = controller.get('identification');
+         var password = controller.get('password');
+
       this.get('session').open('firebase', {
         provider: 'password',
-        email: 'mmiissaann@seznam.cz',
-        password: 'password'}).then(function(data) {
+        email: email,
+        password: password}).then(function(data) {
         console.log(data.currentUser);
       });
+      var controller = this.get('controller');
+      controller.set('signInvisible', false);
+
     },
     signOut: function() {
       this.get('session').close();
     },
     signUp() {
+      var controller = this.get('controller');
+      var email = controller.get('email');
+      var password = controller.get('password');
       let auth = this.get('firebaseApp').auth();
-      auth.createUserWithEmailAndPassword('nova@iquest.cz', 'kytara').then(response => {
+      auth.createUserWithEmailAndPassword(email, password).then(response => {
         /*let user = this.get('user');
         user.set('id', response.uid);
 
@@ -39,6 +61,13 @@ export default Route.extend({
           loading: false, error: this.get('i18n').t(`user.${error.code}`)
         });*/
       });
+      var controller = this.get('controller');
+      controller.set('signUpvisible', false);
+    },
+    modalClose() {
+      var controller = this.get('controller');
+      controller.set('signUpvisible', false);
+      controller.set('signInvisible', false);
     }
   }
 });
