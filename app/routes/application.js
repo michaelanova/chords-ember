08c67619eac0607f54011e9f667bdf962f2d78a0
michaelanova <mmiissaann@seznam.cz>
2017-11-07 14:44:58 +0100

    signUp

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
+  firebaseApp: Ember.inject.service(),
   session: Ember.inject.service(),
   beforeModel: function() {
     return this.get('session').fetch().catch(function() {});
@@ -17,6 +18,27 @@ export default Route.extend({
     },
     signOut: function() {
       this.get('session').close();
+    },
+    signUp() {
+      let auth = this.get('firebaseApp').auth();
+      auth.createUserWithEmailAndPassword('nova@iquest.cz', 'kytara').then(response => {
+        /*let user = this.get('user');
+        user.set('id', response.uid);
+
+        return user.save().then(() => {
+          return this.get('session').fetch().then(() => {
+            this.sendAction('transitionTo', 'challenges');
+          });
+        }, () => {
+          this.setProperties({
+            loading: false, error: this.get('i18n').t('user.submit_error')
+          });
+        });
+      }, error => {
+        this.setProperties({
+          loading: false, error: this.get('i18n').t(`user.${error.code}`)
+        });*/
+      });
     }
   }
 });
